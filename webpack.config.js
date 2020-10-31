24becaa1cc0d8fd2e259ce865c2e8397492758b6
HauresBogdan <hauresbogdan@gmail.com>
2020-10-31 19:41:34 +0200

    webpack strugle2

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,41 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/pagination.jsx',
-    output: {
-        path: path.resolve('dist'),
-        filename: 'pagination.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            }
-        ]
+  mode: 'production',
+  entry: './src/pagination.js',
+  output: {
+    path: path.resolve('lib'),
+    filename: 'pagination.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
     }
-}
+  },
+  externals: {
+    // Don't bundle react or react-dom      
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  }
+};
